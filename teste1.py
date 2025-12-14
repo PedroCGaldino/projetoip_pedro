59ca72dc089478192859a83423bfa1e9aef34fea
Pedro Henrique Cabral Galdino dos Santos <phcgs@cin.ufpe.br>
2025-12-14 19:16:02 -0300

    Add files via upload

---
+++ teste1.py
@@ -0,0 +1,154 @@
+import pygame
+from pygame.locals import *
+from sys import exit
+import os
+
+diretorio_principal = os.path.dirname(__file__)
+diretorio_imagens = os.path.join(diretorio_principal, 'imagens')
+diretorio_sons = os.path.join(diretorio_principal, 'sons')
+
+
+pygame.init()
+
+largura = 1280
+altura = 960
+
+PRETO = (0, 0, 0)
+
+tela = pygame.display.set_mode((largura, altura))
+pygame.display.set_caption("Caramelo: A Saga do Bolo de Rolo Dourado")
+
+class Caramelo(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.sprites = []
+        self.sprites.append(pygame.image.load('sprites/sprite_latido0.png'))
+        self.sprites.append(pygame.image.load('sprites/sprite_latido1.png'))
+        self.sprites.append(pygame.image.load('sprites/sprite_latido2.png'))
+        self.sprites.append(pygame.image.load('sprites/sprite_latido3.png'))
+        self.sprites.append(pygame.image.load('sprites/sprite_latido4.png'))
+        self.andando_sprites = []
+        spritesheet_andando = pygame.image.load('sprites/caramelo_andando.png')
+        frame_width = spritesheet_andando.get_width() // 3
+        frame_height = spritesheet_andando.get_height()
+        for i in range(3):
+            rect = pygame.Rect(i * frame_width, 0, frame_width, frame_height)
+            self.andando_sprites.append(spritesheet_andando.subsurface(rect))
+        self.pulando_sprites = []
+        spritesheet_pulando = pygame.image.load('sprites/caramelo_pulando.png')
+        frame_width_p = spritesheet_pulando.get_width() // 8
+        frame_height_p = spritesheet_pulando.get_height()
+        for i in range(7):
+            rect = pygame.Rect(i * frame_width_p, 0, frame_width_p, frame_height_p)
+            self.pulando_sprites.append(spritesheet_pulando.subsurface(rect))
+        self.atual = 0
+        self.image = self.sprites[self.atual]
+        self.image = pygame.transform.scale(self.image, (120, 120))
+        
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (100, 785)
+
+        self.animar_latir = False
+        self.andando = False
+        self.pulando = False
+        self.direcao = 0
+        self.velocidade = 5
+        self.facing = 1
+        self.velocity_y = 0
+        self.gravity = 0.3
+        self.jump_strength = 10
+        self.on_ground = True
+        self.ground_level = 785 + 120
+
+    def latir(self):
+        self.animar_latir = True
+    def andar(self, direcao):
+        self.andando = True
+        self.direcao = direcao
+        self.facing = direcao
+    def pular(self):
+        if self.on_ground:
+            self.velocity_y = -self.jump_strength
+            self.on_ground = False
+            self.pulando = True
+            self.atual = 0
+    def parar(self):
+        self.andando = False
+    def update(self):
+        if self.animar_latir == True:
+            self.atual = self.atual + 0.05
+            if self.atual >= len(self.sprites):
+                self.atual = 0
+                self.animar_latir = False
+            self.image = self.sprites[int(self.atual)]
+            if self.facing < 0:
+                self.image = pygame.transform.flip(self.image, True, False)
+            self.image = pygame.transform.scale(self.image, (120, 120))
+        elif self.pulando:
+            self.atual = self.atual + 0.1
+            if self.atual >= len(self.pulando_sprites):
+                self.atual = len(self.pulando_sprites) - 1
+            img = self.pulando_sprites[int(self.atual)]
+            if self.facing < 0:
+                img = pygame.transform.flip(img, True, False)
+            self.image = pygame.transform.scale(img, (120, 120))
+        elif self.andando:
+            self.atual = self.atual + 0.05
+            if self.atual >= len(self.andando_sprites):
+                self.atual = 0
+            img = self.andando_sprites[int(self.atual)]
+            if self.direcao < 0:
+                img = pygame.transform.flip(img, True, False)
+            self.image = pygame.transform.scale(img, (120, 120))
+            self.rect.x += self.direcao * self.velocidade
+        else:
+            img = self.sprites[0]
+            if self.facing < 0:
+                img = pygame.transform.flip(img, True, False)
+            self.image = pygame.transform.scale(img, (120, 120))
+        
+        # Physics
+        self.rect.y += self.velocity_y
+        self.velocity_y += self.gravity
+        if self.rect.bottom >= self.ground_level:
+            self.rect.bottom = self.ground_level
+            self.velocity_y = 0
+            self.on_ground = True
+            self.pulando = False
+    
+    
+todas_as_sprites = pygame.sprite.Group()
+caramelo = Caramelo()
+todas_as_sprites.add(caramelo)
+
+imagem_fundo = pygame.image.load('sprites/background.jpg').convert()
+imagem_fundo = pygame.transform.scale(imagem_fundo, (largura, altura))
+
+relogio = pygame.time.Clock()        
+        
+
+while True:
+    relogio.tick()
+    tela.fill((PRETO))
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            exit()
+        if event.type == KEYDOWN and event.key == K_SPACE:
+            caramelo.latir()
+        if event.type == KEYDOWN and event.key == K_w:
+            caramelo.pular()
+
+    keys = pygame.key.get_pressed()
+    if keys[K_a]:
+        caramelo.andar(-1)
+    elif keys[K_d]:
+        caramelo.andar(1)
+    else:
+        caramelo.parar()
+
+    tela.blit(imagem_fundo, (0, 0))
+    todas_as_sprites.draw(tela)
+    todas_as_sprites.update()
+    pygame.display.flip()
+    
